8692b4eb04e26eb6e64d3829026784c33b712696
luisyet <sulcaluis151999@gmail.com>
2024-02-22 06:08:08 -0500

    clean and include bootstrap

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,7 +9,21 @@ import { IPokemon } from '../intefaces/pokemon.interface';
 export class CardComponent {
 
   @Input() pokemon: IPokemon = {} as IPokemon;
+  @Input() numero: number = 0;
 
   ngOnInit(){}
 
+  openModal() {
+    const element = document.getElementById("myModal");
+    if(element){
+      element.style.display = 'block';
+    }
+  }
+
+  closeModal() {
+    const element = document.getElementById("myModal");
+    if(element){
+      element.style.display = 'none';
+    }
+  }
 }
