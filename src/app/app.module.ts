212348518525afd8edf3d912df78a238145bb745
luisyet <sulcaluis151999@gmail.com>
2024-03-04 14:13:16 -0500

    add responsive grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PokemonService } from './pokemon.service';
 import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
 ];
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  year: number = 0;
+
+  ngOnInit() {
+    this.year = (new Date()).getFullYear();
+  }
+
 }
